c078caaa192839e522dd6a0f46fe394f6d58258d
eriinys <eriinys@duck.com>
2025-11-04 21:45:16 -0600

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class Main {
+public class Main{
+    public static void main(String[] args) {
+        List<Person> myFamily = new ArrayList<Person>();
+        myFamily.add(new Person("Dana", "Wyatt", 63));
+        myFamily.add(new Person("Dave", "Dykes", 50));
+        myFamily.add(new Person("Ian", "Auston", 16));
+        myFamily.add(new Person("Aaron","Wyatt", 31));
+        myFamily.add(new Person("Ian", "Auston", 72));
+
+        Collections.sort(myFamily);
+        System.out.println(myFamily);
+    }
+
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+public class Person implements Comparable<Person> {
+    private String firstName;
+    private String lastName;
+    private int age;
 
-public class Person {
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person that) {
+        int compareLastName = this.lastName.compareTo(that.lastName);
+        int compareFirstName = this.firstName.compareTo(that.firstName);
+        if (compareLastName == 0){
+            return compareFirstName;
+        }
+        if (compareLastName == 0 && compareFirstName == 0){
+            if(this.age < that.age) {
+                return -1;
+            }
+            if (this.age > that.age) {
+                return 1;
+            }
+            return 0;
+        }
+        return compareLastName;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s %s, %d", lastName, firstName, age);
+    }
+
 }
